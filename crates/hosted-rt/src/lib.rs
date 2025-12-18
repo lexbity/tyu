0f31298d4c286aa51de5a05fdec3926ad70082c2
prestin liu <liuprestin@gmail.com>
2025-12-17 19:51:37 -0800

    Add crates/hosted-rt/src/lib.rs

---
+++ crates/hosted-rt/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+use hosted::{c, io};
+
+#[no_mangle]
+pub extern "C" fn rust_eh_personality() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    let _ = io::stderr(b"error: panic\n");
+    unsafe { c::_exit(101) }
+}
+
+pub unsafe fn exit(code: i32) -> ! {
+    c::_exit(code)
+}
+
+#[macro_export]
+macro_rules! entry {
+    ($main:path) => {
+        #[no_mangle]
+        pub extern "C" fn main(argc: i32, argv: *const *const hosted::c::c_char) -> i32 {
+            $main(argc as isize, argv)
+        }
+    };
+}
