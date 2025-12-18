af6eba751ff40c43576898aaa0d804d17316be48
prestin liu <liuprestin@gmail.com>
2025-12-17 19:51:37 -0800

    Add crates/hosted/src/cstrbuf.rs

---
+++ crates/hosted/src/cstrbuf.rs
@@ -0,0 +1,39 @@
+use crate::{errno::Errno, mem};
+use core::{ffi::c_void, ptr::NonNull};
+
+pub struct CStrBuf {
+    ptr: NonNull<u8>,
+    len: usize,
+}
+
+impl CStrBuf {
+    pub fn new(bytes: &[u8]) -> Result<Self, Errno> {
+        if bytes.iter().any(|&b| b == 0) {
+            return Err(Errno(22));
+        }
+        let alloc = mem::malloc(bytes.len() + 1)?;
+        let ptr = alloc.cast::<u8>();
+        unsafe {
+            core::ptr::copy_nonoverlapping(bytes.as_ptr(), ptr.as_ptr(), bytes.len());
+            *ptr.as_ptr().add(bytes.len()) = 0;
+        }
+        Ok(Self {
+            ptr,
+            len: bytes.len(),
+        })
+    }
+
+    pub fn as_ptr_i8(&self) -> *const i8 {
+        self.ptr.as_ptr() as *const i8
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl Drop for CStrBuf {
+    fn drop(&mut self) {
+        unsafe { mem::free(self.ptr.cast::<c_void>()) };
+    }
+}
