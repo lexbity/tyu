4b8ee0761a8367faf75edc019351109314db9411
prestin liu <liuprestin@gmail.com>
2025-12-17 19:51:37 -0800

    Add crates/frontend/src/span.rs

---
+++ crates/frontend/src/span.rs
@@ -0,0 +1,12 @@
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Span {
+    pub const fn new(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+}
+
